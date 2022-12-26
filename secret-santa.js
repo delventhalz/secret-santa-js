2f5fba817ea11514146ddbd5300b42feafb81c17
Zac Delventhal <delventhalz@gmail.com>
2022-12-26 12:21:47 -0500

    Parse santas config into indexed Map

--- secret-santa.js
+++ secret-santa.js
@@ -2,4 +2,68 @@
 
 const config = require('./config.json');
 
-console.log(config);
+const EMAIL_PATTERN = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i;
+
+const randInt = max => Math.floor(Math.random() * max);
+
+const shuffle = (items) => {
+  const source = items.slice();
+  const shuffled = [];
+
+  while (source.length > 0) {
+    const index = randInt(source.length);
+    shuffled.push(...source.splice(index, 1));
+  }
+
+  return shuffled;
+};
+
+const toSantaMap = (santas) => {
+  const map = new Map();
+
+  for (const santa of santas) {
+    if (map.has(santa.name)) {
+      throw new Error(`Santa name is not unique: ${santa.name}`);
+    }
+
+    map.set(santa.name, santa);
+  }
+
+  return map;
+};
+
+const validateSantaMap = (santaMap) => {
+  for (const { email, blocked = [], always = [] } of santaMap.values()) {
+    if (!EMAIL_PATTERN.test(email)) {
+      throw new Error(`Invalid email address: ${email}`);
+    }
+
+    for (const blockedName of blocked) {
+      if (!santaMap.has(blockedName)) {
+        throw new Error(`Cannot find blocked name: ${blockedName}`);
+      }
+    }
+
+    for (const alwaysName of always) {
+      if (!santaMap.has(alwaysName)) {
+        throw new Error(`Cannot find always name: ${alwaysName}`);
+      }
+    }
+  }
+};
+
+const validateGroups = (santaMap, groups) => {
+  for (const group of groups) {
+    for (const name of group) {
+      if (!santaMap.has(name)) {
+        throw new Error(`Cannot find group name: ${name}`);
+      }
+    }
+  }
+};
+
+const santaMap = toSantaMap(config.santas);
+validateSantaMap(santaMap);
+validateGroups(santaMap, config.groups);
+
+console.log(santaMap);
